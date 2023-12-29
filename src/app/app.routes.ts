59374d851a27d6c3f9b90444d2cf20b275722e1e
MargaritaAlvarezGonz <margarita.alvarez3105@gmail.com>
2023-12-29 02:44:01 -0600

    added routerlink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { BlogPostComponent } from './components/blog-post/blog-post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+  path:'blog-post',
+  title:'Blog',
+  component: BlogPostComponent,
+}
+
+];
