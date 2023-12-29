8ec2e7d4843909ab76be8535f47bc0071a9af158
MargaritaAlvarezGonz <margarita.alvarez3105@gmail.com>
2023-12-29 03:17:30 -0600

    added new rective form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { BlogPostComponent } from './components/blog-post/blog-post.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
 {
   path:'blog-post',
   title:'Blog',
   component: BlogPostComponent,
+},
+{
+  path:'contact',
+  title:'Contact form',
+  component: ContactComponent,
 }
 
 ];
